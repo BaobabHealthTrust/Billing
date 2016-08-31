d3ecc38cbe17c1be3451db7707add1739e9e1e70
Jacob Mziya <jacobmziya@gmail.com>
2016-08-31 16:15:01 +0200

    User controller.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -18,7 +18,8 @@ class UserController < ApplicationController
 		if password == password_confirmation
 			user_role_id = UserRole.find(role_id).id
 			new_user = User.new(username: username, password: password,
-			                    password_confirmation: password_confirmation, user_role_id: user_role_id)
+			                    password_confirmation: password_confirmation,
+			                    user_role_id: user_role_id)
 			if new_user.valid?
 				new_user.save
 				redirect_to '/'
